94acf1015da82d833292e470a93eefdaf5af096f
zhujun wang <zwang94@mail.depaul.edu>
2025-07-21 22:32:44 -0400

    ready for releas 1.0.1

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -37,6 +37,9 @@ def go(args):
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
 
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Save cleaned data to a new file
     cleaned_path = "clean_sample.csv"
     df.to_csv(cleaned_path, index=False)
